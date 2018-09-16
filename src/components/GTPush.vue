bba55b23212e6c018c0a393ce28b422ec7ec1650
qingw <qingw@jumei.com>
2018-09-16 12:32:10 +0800

    1. 添加个推页面。

---
+++ src/components/GTPush.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      我的是个推推送页面。
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GTPush"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import GtPush from '@/components/GtPush'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'GtPush',
+      component: GtPush
     }
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // }
   ]
 })
