e5c356352d0814ade4c7d59abd668da0243af4cd
WangQing <wangqing11@100tal.com>
2019-06-15 14:10:50 +0800

    1. 修改上传的目录。

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-distDir=/root/webroot
+distDir=/home/WangQing/webroot
 currentDir=/Users/WangQing/VUE_Pro/JMVue/dist
 scp -r $currentDir/index.html $currentDir/static root@zkteam.cc:$distDir
